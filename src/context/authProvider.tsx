25f164cdfe0726c26f8087ecf0220e6b2dad9698
ajmal-j <ajmaljaleel7860@gmail.com>
2023-12-29 15:13:18 +0530

    done auth provider using create context

--- src/context/authProvider.tsx
+++ src/context/authProvider.tsx
@@ -1,12 +1,49 @@
-import { ReactNode, createContext, useContext } from "react";
-import {auth} from'../auth/firebase';
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { auth, app } from "../auth/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
 
 const AuthContext = createContext<any>(undefined);
 
-export function AuthProvider({ children }: { children: ReactNode }) {  
-  return <AuthContext.Provider value={'d'}>{children}</AuthContext.Provider>;
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState({});
+
+  function signUp(email: string, password: string) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+  function logIn(email: string, password: string) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+  function logOut() {
+    return signOut(auth);
+  }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser: any) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  });
+  return (
+    // @ts-ignore
+    <AuthContext.Provider value={(signUp, logOut, logIn, user)}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-export function UserAuth(){
-    return useContext(AuthContext)
+export function UserAuth() {
+  return useContext(AuthContext);
 }
